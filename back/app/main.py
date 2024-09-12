89e535f68f87224bbbe4baae2bcb32dd73e1ad37
JochiBaez1 <jochi_perez2010@hotmail.com>
2024-09-12 09:58:00 -0400

    yo creo que eto ta bien

--- back/app/main.py
+++ back/app/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI, Request, HTTPException
-from fastapi.templating import Jinja2Templates
 from starlette.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 import IP2Location
@@ -34,10 +33,6 @@ async def github_webhook(x_github_token: str = Header(None)):
 
 
 
-# Configura la carpeta donde estarán tus templates de Jinja2
-templates = Jinja2Templates(directory="template")
-#app.mount("/asset", StaticFiles(directory="asset"), name="asset")
-
 def check_invalid_ip(rec):
     """Verifica si hay un valor de IP inválido en el diccionario."""
     for key, value in rec.items():
@@ -46,43 +41,7 @@ def check_invalid_ip(rec):
         elif "-" in value:
             raise HTTPException(status_code=404, detail="Resource not found")
 
-@app.get("/", response_class=HTMLResponse)
-async def home(request: Request):
-    client_ip = request.client.host  # Captura la IP del cliente
 
-    # Verifica si hay un encabezado 'X-Forwarded-For' para capturar la IP real detrás de un proxy
-    forwarded_for = request.headers.get('X-Forwarded-For')
-    if forwarded_for:
-        client_ip = forwarded_for.split(',')[0].strip()  # La primera IP es la real en caso de múltiples proxies
-    
-    print(client_ip)
-    
-    try:
-        if len(client_ip) > 15:
-            # Base de datos para IPs IPv6
-            database = IP2Location.IP2Location('./app/DB1_IP6.BIN')
-            rec = database.get_all(client_ip).__dict__
-            
-            # Verificar valores inválidos
-            check_invalid_ip(rec)
-            
-            print("Base de datos IP6")
-        else:
-            # Base de datos para IPs IPv4
-            database = IP2Location.IP2Location('./app/IP2LOCATION-LITE-DB3.BIN')
-            rec = database.get_all(client_ip).__dict__
-            
-            print("Base de datos IP4")
-        
-    except HTTPException as e:
-        # Manejar excepciones específicas
-        raise e
-    except Exception as e:
-        # Manejar otros errores generales
-        print(f"Error: {e}")  # Opcional, para depuración
-        raise HTTPException(status_code=404, detail="Resource not found")
-
-    return templates.TemplateResponse("index.html", {"request": request, "client_ip": client_ip, "rec": rec})
 
 @app.get("/get-ip")
 async def get_ip(request: Request, ip: str):
