8d516d1d06a88b58770b65856683376268a75127
daviddev <davidtejadamoreta26@gmail.com>
2024-09-15 15:10:06 -0400

    Implementacion de redis como servidor de cache

--- back/app/main.py
+++ back/app/main.py
@@ -1,3 +1,4 @@
+import time  # Importamos el módulo time para medir el tiempo
 from fastapi import FastAPI, Request, HTTPException
 from starlette.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
@@ -5,8 +6,10 @@ from fastapi.middleware.trustedhost import TrustedHostMiddleware
 from fastapi.middleware.cors import CORSMiddleware
 import IP2Location
 import os
-from fastapi import FastAPI, Header, HTTPException, status
-
+import redis
+import json
+from app.configDB import redis_client
+from fastapi import Header, HTTPException, status
 
 app = FastAPI()
 
@@ -23,12 +26,9 @@ app.add_middleware(
     allowed_hosts=["*"],
 )
 
-
 token = os.environ['TOKEN_GITHUB']
-# Define un token estático que GitHub debe enviar
 GITHUB_TOKEN = token
 
-# Verifica si el token en las solicitudes es válido
 def verify_github_token(token: str):
     if token != GITHUB_TOKEN:
         raise HTTPException(
@@ -39,57 +39,59 @@ def verify_github_token(token: str):
 
 @app.post("/auth")
 async def github_webhook(x_github_token: str = Header(None)):
-    # Verifica el token
     verify_github_token(x_github_token)
-    
-    # Aquí procesarías el evento de GitHub
     return {"message": "Solicitud válida de GitHub"}
 
-
-
-
-
-def check_invalid_ip(rec):
-    """Verifica si hay un valor de IP inválido en el diccionario."""
-    for key, value in rec.items():
+def check_invalid_ip(data):
+    for key, value in data.items():
         if "INVALID IP ADDRESS" in value:
             raise HTTPException(status_code=400, detail="Invalid IP address value in the record")
         elif "-" in value:
             raise HTTPException(status_code=404, detail="Resource not found")
 
-
-
 @app.get("/get-ip")
 async def get_ip(request: Request, ip: str):
     try:
-        if len(ip) > 15:
-            # Base de datos para IPs IPv6
-            database = IP2Location.IP2Location('./app/DB1_IP6.BIN')
-            rec = database.get_all(ip).__dict__
-            
-            # Verificar valores inválidos
-            check_invalid_ip(rec)
-            
-            print("Base de datos IP6")
+        # Medir el tiempo de consulta a Redis
+        redis_start_time = time.time()
+        
+        data = redis_client.get(ip)
+        
+        redis_end_time = time.time()
+        redis_duration = redis_end_time - redis_start_time
+        print(f"Tiempo de respuesta de Redis: {redis_duration:.6f} segundos")
+        
+        if data is not None:
+            data = json.loads(data.decode("UTF-8"))
         else:
-            # Base de datos para IPs IPv4
-            database = IP2Location.IP2Location('./app/IP2LOCATION-LITE-DB3.BIN')
-            rec = database.get_all(ip).__dict__
+            # Si no se encuentra en Redis, medir el tiempo de consulta a la base de datos
+            db_start_time = time.time()
             
-            # Verificar valores inválidos
-            check_invalid_ip(rec)
+            if len(ip) > 15:
+                # Base de datos para IPs IPv6
+                database = IP2Location.IP2Location('./app/DB1_IP6.BIN')
+                data = database.get_all(ip).__dict__
+                print("Base de datos IP6")
+            else:
+                # Base de datos para IPs IPv4
+                database = IP2Location.IP2Location('./app/IP2LOCATION-LITE-DB3.BIN')
+                data = database.get_all(ip).__dict__
+                print("Base de datos IP4")
             
-            print("Base de datos IP4")
-    
+            # Fin de la consulta a la base de datos
+            db_end_time = time.time()
+            db_duration = db_end_time - db_start_time
+            print(f"Tiempo de respuesta de la base de datos: {db_duration:.6f} segundos")
+            
+            # Almacenar en Redis para futuras consultas
+            redis_client.set(ip, json.dumps(data))
+
+        check_invalid_ip(data)
+        
     except HTTPException as e:
-        # Manejar excepciones específicas
         raise e
     except Exception as e:
-        # Manejar otros errores generales
-        print(f"Error: {e}")  # Opcional, para depuración
+        print(f"Error: {e}")
         raise HTTPException(status_code=404, detail="Resource not found")
     
-    return {"ip": rec}
-
-
-
+    return {"ip": data}
